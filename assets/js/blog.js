defa88de9ae54aabbec2a5348f9fb75fb9823d4e
Victor Roman <Romantech91>
2024-11-03 18:32:30 -0700

    updated blog.js for back button to work

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,26 +1,26 @@
-const mainElement = document.querySelector('#blog-posts');
+const mainElement = document.querySelector("#blog-posts");
 
 const displayBlogPosts = () => {
-  const posts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+  const posts = JSON.parse(localStorage.getItem("blogPosts")) || [];
 
   if (posts.length === 0) {
-    const noPostsMessage = document.createElement('p');
+    const noPostsMessage = document.createElement("p");
     noPostsMessage.textContent = "No Blog posts yet...";
     mainElement.appendChild(noPostsMessage);
     return;
   }
 
-  posts.forEach(post => {
-    const postContainer = document.createElement('div');
-    const postTitle = document.createElement('h2');
-    const postAuthor = document.createElement('p');
-    const postContent = document.createElement('p');
+  posts.forEach((post) => {
+    const postContainer = document.createElement("div");
+    const postTitle = document.createElement("h2");
+    const postAuthor = document.createElement("p");
+    const postContent = document.createElement("p");
 
     postTitle.textContent = post.title;
     postAuthor.textContent = `By: ${post.username}`;
     postContent.textContent = post.content;
 
-    postContainer.classList.add('post');
+    postContainer.classList.add("post");
     postContainer.appendChild(postTitle);
     postContainer.appendChild(postAuthor);
     postContainer.appendChild(postContent);
@@ -28,5 +28,11 @@ const displayBlogPosts = () => {
     mainElement.appendChild(postContainer);
   });
 };
+
+const backButton = document.getElementById("back");
+
+backButton.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
 
 displayBlogPosts();
